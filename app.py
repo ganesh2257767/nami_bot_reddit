36781b549b5c895e393b8b236e0533a6a53f59b8
Ganesh Kutty <ganeshkutty2257767@gmail.com>
2023-03-24 00:19:12 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import praw
+import random
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id = os.environ.get('client_id')
+client_secret = os.environ.get('client_secret')
+password = os.environ.get('reddit_pass')
+username = os.environ.get('reddit_user')
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    password=password,
+    username=username,
+    user_agent="<Nami Wants Money 0.1>"
+)
+subreddit = reddit.subreddit('MemePiece')
+
+triggers = ['money', 'gold', 'treasure']
+replies = ["Give me all your {}!!!", "I love {}!!!", "Did you say {}?!! Can I have it?", "Sounds good, let me have it!"]
+
+for comment in subreddit.stream.comments(skip_existing=True):
+    if comment.author != 'NamiWantsMoney':
+        trigger_match = [x.upper() for x in triggers if x in comment.body.lower()]
+        if trigger_match:
+            i = ' & '.join([', '.join(trigger_match[:-1]), trigger_match[-1]]) if len(trigger_match) > 1 else trigger_match[0]
+            reply = random.choice(replies).format(i)
+            comment.reply(reply)
